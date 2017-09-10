fa5dcf62b422f24000710e757f62a459df795afc
Kaioru <keifuurawrs@gmail.com>
2017-09-10 10:40:22 +0800

    deskly-cli 1.10

--- Formula/deskly-cli.rb
+++ Formula/deskly-cli.rb
@@ -3,9 +3,9 @@ require "language/node"
 class DesklyCli < Formula
   desc "the core of a simple reddit desktop image changer"
   homepage "https://github.com/Deskly/deskly-cli#readme"
-  url "https://registry.npmjs.org/deskly-cli/-/deskly-cli-1.0.1.tgz"
-  version "1.0.1"
-  sha256 "5e88f2ac16db836676326549d17029bcaac3e75f3fd69ee42f0a5116f87ab33a"
+  url "https://registry.npmjs.org/deskly-cli/-/deskly-cli-1.1.0.tgz"
+  version "1.1.0"
+  sha256 "2d3f7fd79e25f3c4371a951d05f0adef7fea0f9eb87030eb58a4ff1022d9f6a4"
 
   depends_on "node"
 
