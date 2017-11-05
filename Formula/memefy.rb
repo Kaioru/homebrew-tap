4605dae0816096bdd5c0e0e4701040a247f1d4c4
Kaioru <keifuurawrs@gmail.com>
2017-11-05 19:03:12 +0800

    memefy 1.1.2

--- Formula/memefy.rb
+++ Formula/memefy.rb
@@ -3,9 +3,9 @@ require "language/node"
 class Memefy < Formula
   desc "pronounced as me me fy. yes really. dont laugh"
   homepage "https://github.com/Kaioru/memefy.js#readme"
-  url "https://registry.npmjs.org/memefy/-/memefy-1.1.0.tgz"
-  version "1.1.0"
-  sha256 "82b913d2d87f03c227da7be955ee25974aa600e3d2691957f5115bdaa0c86975"
+  url "https://registry.npmjs.org/memefy/-/memefy-1.1.2.tgz"
+  version "1.1.2"
+  sha256 "003a710d6c72b23a5f0a56c6de8cbf9fd250e4a207d78b79bc1581e02921e803"
 
   depends_on "node"
 
